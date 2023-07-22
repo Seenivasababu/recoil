88163a7dce8aa10784efdb19c8cc54a0787883da
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-07-22 23:24:14 +0530

    Recoil intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
+import { Button, Card,Typography } from '@mui/material';
 import './App.css';
+import { RecoilRoot , atom, useRecoilValue, useSetRecoilState} from 'recoil';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div style={{ display:'flex', justifyContent:'center'}}>
+          <Card style={{ margin:60, padding:20, width:500}}> 
+              <Typography variant='h5' padding={2} textAlign={'center'}> Welcome To counter game </Typography>
+              <Buttons />
+              <CountComponent />  
+          </Card>
+      </div>
+    </RecoilRoot>
   );
 }
 
+const Buttons = () =>{
+  return (
+  <div style={{ display:'flex', justifyContent:'space-between' }}>
+    <Increment/>
+    <Decrease/>
+  </div>)
+}
+
+const Increment = () =>{
+  const setCount = useSetRecoilState(countState)
+  return <div>
+    <Button variant='contained' onClick={()=>{setCount(e=>e+1)}}>
+      Increase Cunter
+    </Button>
+  </div>
+}
+const Decrease = () =>{
+  const setCount = useSetRecoilState(countState)
+  return <div>
+  <Button variant='contained' onClick={()=>{setCount(e=>e-1)}}>
+    Decrease Cunter
+  </Button>
+</div>
+}
+const CountComponent = () =>{
+  const count = useRecoilValue(countState)
+  return <div>
+      <Typography variant='h5' textAlign={'center'}> {count} </Typography>
+  </div>
+}
+
+
 export default App;
+
+const countState = atom({
+  key:'countState',
+  default:0
+})
